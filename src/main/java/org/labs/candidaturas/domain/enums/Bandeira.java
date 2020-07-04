e2b39eb9519710870da0470b7cb46458c3f8faef
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-07-04 17:13:10 -0300

    Implementando os Enums Nivel e Bandeira

--- src/main/java/org/labs/candidaturas/domain/enums/Bandeira.java
+++ src/main/java/org/labs/candidaturas/domain/enums/Bandeira.java
@@ -2,5 +2,17 @@ package org.labs.candidaturas.domain.enums;
 
 public enum Bandeira {
 	
+	MASTERCARD("Master Card"), 
+	VISA("Visa"), 
+	AMERICANEXPRESS("American Express"), 
+	DINERSCLUB("Diners Club"), 
+	DISCOVER("Discover"), 
+	VOYAGER("Voyager"), 
+	HIPERCARD("Hiper Card"), 
+	AURA("Aura");
+	
+	private String value;
+	
+	Bandeira(String value) { this.value = value; }
 	
 }
--- src/main/java/org/labs/candidaturas/domain/enums/Nivel.java
+++ src/main/java/org/labs/candidaturas/domain/enums/Nivel.java
@@ -1,5 +1,13 @@
 package org.labs.candidaturas.domain.enums;
 
 public enum Nivel {
-
+	BASICO("Básico"),
+	JUNIOR("Júnior"),
+	PLENO("Pleno"),
+	AVANCADO("Avançado"),
+	EXPERT("Expert");
+	
+	private String value;
+	
+	Nivel(String value) { this.value = value; }
 }
